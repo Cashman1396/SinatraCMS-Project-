d8089445c5c456efdaa2ea1ec88d403f824d0caf
Chris Ashman <chrisashman96@gmail.com>
2020-04-27 20:37:04 +0000

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "hidden secret"
   end
 
+  #redirects to index if session is empty
   get "/" do
-    erb :welcome
+    if session[:user_id].nil?
+      erb :index
+        redirect '/home'
+      end
   end
 
+  #helpers added to find current user and session
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
